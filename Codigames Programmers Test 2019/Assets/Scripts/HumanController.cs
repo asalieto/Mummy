78e9c8727aa89902b682252a0164d5492ab826dc
Alberto Salieto <sithis77@gmail.com>
2019-03-03 19:46:06 +0100

    - Added PlayerController to control the tap-and-move beahaviour - Added HumanController. - NavMesh on the game scene.

---
+++ Codigames Programmers Test 2019/Assets/Scripts/HumanController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class HumanController : MonoBehaviour
+{
+    [SerializeField]
+    private Animator m_animator;
+    [SerializeField]
+    private NavMeshAgent m_navMeshAgent;
+
+    private bool m_isMoving;
+
+    public void Init()
+    {
+        m_animator.SetBool("Grounded", true);
+    }
+
+    public void MoveTo(Vector3 position)
+    {
+        // Debug.Log("MoveTo: " + position);
+        m_isMoving = true;
+
+        m_navMeshAgent.destination = position;
+        m_navMeshAgent.isStopped = false;
+
+        m_animator.SetFloat("MoveSpeed", 15f);
+    }
+    
+    private void Update()
+    {
+        if (m_isMoving)
+        {
+            if (m_navMeshAgent.remainingDistance <= m_navMeshAgent.stoppingDistance)
+            {
+                if (!m_navMeshAgent.hasPath || Mathf.Abs(m_navMeshAgent.velocity.sqrMagnitude) < float.Epsilon)
+                {
+                    m_isMoving = false;
+                    m_animator.SetFloat("MoveSpeed", 0f);
+                }
+            }
+            else
+            {
+                m_isMoving = true;
+            }
+        }
+    }
+}
---
+++ Codigames Programmers Test 2019/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private List<HumanController> m_humans;
+    [SerializeField]
+    private float m_maxRandomRange;
+
+    private Ray ray;
+    private RaycastHit hit;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    public void Init()
+    {
+        for (int i = 0; i < m_humans.Count; i++)
+        {
+            if (m_humans[i] != null)
+            {
+                m_humans[i].Init();
+            }
+        }
+    }
+
+    private void MoveHumans(Vector3 position)
+    {
+        float variationX, variationZ;
+        Vector3 destination = new Vector3();
+
+        for (int i = 0; i < m_humans.Count; i++)
+        {
+            if (m_humans[i] != null)
+            {
+                variationX = Random.Range(-m_maxRandomRange, m_maxRandomRange);
+                variationZ = Random.Range(-m_maxRandomRange, m_maxRandomRange);
+
+                destination.x = position.x + variationX;
+                destination.y = position.y;
+                destination.z = position.z + variationZ;
+
+                m_humans[i].MoveTo(destination);
+            }
+        }
+    }
+    
+    private void Update()
+    {
+        if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) || (Input.GetMouseButtonDown(0)))
+        {
+
+#if UNITY_EDITOR
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+#elif (UNITY_ANDROID || UNITY_IPHONE)
+            ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+#endif
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Vector3 destination = hit.point;
+                MoveHumans(destination);
+            }
+        }
+    }
+}
