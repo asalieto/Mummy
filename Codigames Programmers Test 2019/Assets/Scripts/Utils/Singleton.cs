40f92316e3e9ebf091ee094c021f2f6f33118c00
Alberto Salieto <sithis77@gmail.com>
2019-03-07 23:22:40 +0100

    - Added singleton class.

---
+++ Codigames Programmers Test 2019/Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : class, new()
+{
+    public static T Instance 
+    {
+        get 
+        {
+            return m_instance;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if (m_instance == null)
+        {
+            m_instance = this as T;
+        }
+        else
+        {
+            Debug.Log(name + ": Error: already initialized");
+        }
+    }
+
+    private static T m_instance = null;
+}
